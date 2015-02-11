86004ec316c802fcfaa29dbc30561bf8be41468a
susannalindskog <susannalindskog@Susannas-MacBook-Pro.local>
2015-02-11 22:45:59 +0100

    hej fulvio

---
+++ page-about.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ Template Name: About
+ *
+ * This is your custom page template. You can create as many of these as you need.
+ * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * top, the same way it is here.
+ *
+ * When you create your page, you can just select the template and viola, you have
+ * a custom page template to call your very own. Your mother would be so proud.
+ *
+ * For more info: http://codex.wordpress.org/Page_Templates
+*/
+?>
+
+<?php get_header(); ?>
+<section id="bg-about-pic">
+
+</section>
+
+	
+	<section id="bg-about-1">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2">
+					<h1 class="about-h1">| Mission |</h1>
+					<p class="about-p">trAmp è un'agenzia giovane ma di esperienza, con sede a Roma. Che affonda le sue radici nel mondo della comunicazione
+						e degli eventi, interpretati in chiave contemporanea e sperimentale, secondo i linguaggi e le necessità del mercato attuale. <br>
+						Il carattere distintivo di trAmp è rappresentato dalla relazione continua instaurata con il pubblico nell'ambito degli eventi di intrattenimento
+						proposti settimanalmente. Vera fonte di ricerca e rinnovamento dell'agenzia.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section id="bg-about-2">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+					<h1 class="about-h1">| Services |</h1>
+					<ul class="about-ul">
+						<li>Produzione e gestione di eventi</li>
+						<li>Servizi per eventi</li>
+						<li>Consulenze per attività di intrattenimento food & beverage</li>
+						<li>Creatività e direzione artistica</li>
+						<li>Pubbliche relazioni targhettizzate</li>
+						<li>Comunicazione digitale</li>
+						<li>Strategie di marketing</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section id="bg-about-">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+					<h1 class="about-h1">| Spin-off |</h1>
+					<p class="about-p">
+						Nell'ambito dei servizi di comunicazione e di contenuto offerti da trAmp srl, in seno all'organizzazione di un evento o al
+						lancio di un prodotto, la produzione video riveste un ruolo centrale, quale chiave di linguaggio e storytelling di immediato
+						riscontro. E' in questa ottica che l'agenzia ha sviluppato una divisione specializzata nella realizzazione di contenuti 
+						audiovisivi emozionali: Showoff produzioni. 
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+<?php get_footer(); ?>
---
+++ page-clients.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ Template Name: Clients
+ *
+ * This is your custom page template. You can create as many of these as you need.
+ * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * top, the same way it is here.
+ *
+ * When you create your page, you can just select the template and viola, you have
+ * a custom page template to call your very own. Your mother would be so proud.
+ *
+ * For more info: http://codex.wordpress.org/Page_Templates
+*/
+?>
+
+<?php get_header(); ?>
+
+<section id="bg-clients">
+	
+</section>
+
+<div id="content">
+	
+	<div class="container-fluid">
+		<h1>| our clients |</h1>
+		<h3 style="text-align: center;">CATEGORY 1</h3>
+		<div class="row">
+			<div class="col-clients col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0">
+
+				<ul class="clients-ul">
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/001.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/002.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/003.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/004.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/005.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/006.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/007.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/008.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/009.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/010.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/011.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/012.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/013.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/014.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/015.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/016.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/017.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/018.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/019.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/020.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/021.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/022.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/023.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/024.png"></li>
+					<li><img class="img-responsive clients-img" src="<?php echo get_template_directory_uri(); ?>/library/clients/025.png"></li>
+				</ul>
+
+			</div>
+		</div>
+	</div>
+	
+
+</div>
+
+
+<?php get_footer(); ?>
